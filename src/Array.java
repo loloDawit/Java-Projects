2fb3cb3745ff120534288498a678a54c0d0968ae
Dave <dawitabera64@gmail.com>
2019-06-28 21:00:04 -0700

    Added binary search method

--- src/Array.java
+++ src/Array.java
@@ -1,7 +1,10 @@
+import org.jetbrains.annotations.Contract;
+
 public class Array {
     private  int[] array;
     private  int nElements;
 
+    @Contract(pure = true)
     public Array(int max){
         array = new int [max];
         nElements = 0;
@@ -12,6 +15,45 @@ public class Array {
             System.out.print(array[i] + " ");
         }
         System.out.println();
+    }
+    public  void  insert(int value){
+        array[nElements] = value;
+        nElements++;
+    }
+    public void delete(int key){
+        int i;
+        for(i=0; i<nElements; i++){
+            if(array[i] == key);
+            break;
+        }
+        if(i == nElements){
+            System.out.println("Opss... can't find " + key);
+        }else {
+            for(int k=i; k<nElements;k++){
+                array[k] = array[k+1];
+                nElements--;
+            }
+        }
+        display();
+    }
+    public  int binarySearch(int keySearch){
+        int lowerBound = 0;
+        int upperBound = nElements -1;
+        int currentIndex;
+        while (true){
+            currentIndex = (lowerBound + upperBound)/2;
+            if(array[currentIndex] == keySearch){
+                return array[currentIndex];
+            }else if(lowerBound > upperBound){
+                return nElements;
+            }else {
+                if(array[currentIndex] < keySearch){
+                    lowerBound = currentIndex + 1;
+                }else {
+                    upperBound =currentIndex -1;
+                }
+            }
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Array array = new Array(100);
+        array.insert(10);
+        array.insert(20);
+        array.insert(1);
+        array.insert(2);
+        array.display();
+        int key = 10;
+        int found = array.binarySearch(key);
+        if(found == key){
+            System.out.println("Found "+found);
+        }else{
+            System.out.println("Opps...");
+        }
+
     }
 }
